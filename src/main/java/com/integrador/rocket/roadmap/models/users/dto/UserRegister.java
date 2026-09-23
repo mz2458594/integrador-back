2c8b4992db7f24bc16f2d2583dc735357038b45a
mz2458594 <mz2458594@gmail.com>
2026-09-22 20:06:58 -0500

    Configuracion de DTO para contraseña y email

--- src/main/java/com/integrador/rocket/roadmap/models/users/dto/UserRegister.java
+++ src/main/java/com/integrador/rocket/roadmap/models/users/dto/UserRegister.java
@@ -1,14 +1,16 @@
 package com.integrador.rocket.roadmap.models.users.dto;
 
 import com.integrador.rocket.roadmap.models.users.Role;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public record UserRegister(
-        @NotBlank String name,
-        @NotBlank String email,
+        @NotBlank(message = "El nombre es requerido") String name,
+        @NotBlank(message = "El email es requerido") @Email(message = "Por favor ingrese un email válido") String email,
         //NOT BLANK NO FUNCIONA CON ENUMS - VALIDAR SI CAMBIA ESTE PARÁMETRO
         Role role,
-        @NotBlank String password
+        @NotBlank(message = "La contraseña es requerida") @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!\\-_]).{8,}$",
+                message = "La contraseña debe tener al menos 8 caractéres, una mayúscula, una minúscula, un número y un caractér especial"
+        ) String password
 ) {
 }
--- src/main/java/com/integrador/rocket/roadmap/models/userstepprogress/dto/UserStepProgressUpdate.java
+++ src/main/java/com/integrador/rocket/roadmap/models/userstepprogress/dto/UserStepProgressUpdate.java
@@ -5,6 +5,5 @@ import jakarta.validation.constraints.NotNull;
 
 public record UserStepProgressUpdate(
         @NotNull Status status
-
 ) {
 }
